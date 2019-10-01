e41b89143bc75d3fad53dc205ee35772a85acaea
Eldiar <55989382+Eldiar36@users.noreply.github.com>
2019-10-01 10:17:36 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $(".slider").owlCarousel({
+        items: 1,
+        loop:true,
+        nav:true,
+        navText:false,
+    });
+});
+$(document).ready(function(){
+    $(".post-slide").owlCarousel({
+        items: 3,
+        loop:true,
+        nav:true,
+        navText:false,
+        margin:10
+    });
+});
+
+
